6d0b53c170d1c3f835a939e0e038889ddb0baedc
Huanying04 <ken911230@gmail.com>
2020-04-24 20:17:40 +0800

    Test

---
+++ app/src/main/java/net/nekomura/newrandomanswer/Setting.kt
@@ -0,0 +1,13 @@
+package net.nekomura.newrandomanswer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Spinner
+
+class Setting : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.setting_dialogue)
+    }
+}
